afc3133c678892efa9279eae48f4ef8a89d69a02
Your Name <you@example.com>
2019-07-14 12:51:33 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    #resp.write "Hello, World"
+    resp.write "Hello, my name is Amritpal Singh"
     resp.finish
   end
 
